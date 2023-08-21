3f4d6738a685bac3b09d700ca96039f1540e80a4
Hasnan <monirhasnan@gmail.com>
2023-08-21 20:15:26 +0600

    ..

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
