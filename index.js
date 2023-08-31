f958772890c9523987b9629e0d3beb7e8ec5cedf
Hasnan <monirhasnan@gmail.com>
2023-08-31 12:15:38 +0600

    dlete recheck added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const cors = require("cors");
 
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
@@ -14,29 +14,33 @@ app.use(cors());
 
 app.use(express.json());
 
-const  verifyJwt = (req,res,next) => {
+const verifyJwt = (req, res, next) => {
   const authorization = req.headers.authorization;
-    if(!authorization) {
-      return res.status(401).send({error: true, message: 'not verify parson'})
-    }
-    const token = authorization.split(' ')[1];
-    jwt.verify(token,'c8f7271005d7cb49f09e71f9f24e8989f0126396a8a88cc1e232f15a130bf83e964e120940702b373c6fe477f69e1d9aa744e58dfc420c50f2794c424c688d91',(err,decoded) => {
-      if(err) {
-        return res.status(401).send({error: true, massage: 'not verify parson'})
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: "not verify parson" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(
+    token,
+    "c8f7271005d7cb49f09e71f9f24e8989f0126396a8a88cc1e232f15a130bf83e964e120940702b373c6fe477f69e1d9aa744e58dfc420c50f2794c424c688d91",
+    (err, decoded) => {
+      if (err) {
+        return res
+          .status(401)
+          .send({ error: true, massage: "not verify parson" });
       }
       req.decoded = decoded;
-      next()
-    })
-
-}
-
+      next();
+    }
+  );
+};
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const { error } = require("console");
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pyqmcvy.mongodb.net/?retryWrites=true&w=majority`; //use monir
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ir3lm70.mongodb.net/?retryWrites=true&w=majority`; //use habib 
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ir3lm70.mongodb.net/?retryWrites=true&w=majority`; //use habib
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 
@@ -56,34 +60,37 @@ async function run() {
 
     // await client.connect();
 
-
-    const resultCollection = client.db("Eresult").collection("resultCollection"); //for getting all results
+    const resultCollection = client
+      .db("Eresult")
+      .collection("resultCollection"); //for getting all results
     const reCheckCollection = client.db("Eresult").collection("reCheck"); //for getting all rechecks
     const usersCollection = client.db("Eresult").collection("usersCollection"); //for getting all rechecks
 
     app.get("/allResults", async (req, res) => {
       const cursor = resultCollection.find();
-      const result = await cursor.toArray()
+      const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
     // search by id
 
     app.get("/allResults/:id", async (req, res) => {
       const allResultsId = req.params.id;
-      const query = { _id: new ObjectId(allResultsId) }
+      const query = { _id: new ObjectId(allResultsId) };
       const option = {
         projection: {
-          Name: 1, classId: 1, finalTerm: 1
-        }
-      }
+          Name: 1,
+          classId: 1,
+          finalTerm: 1,
+        },
+      };
       const results = await resultCollection.findOne(query, option);
       res.send(results);
     });
 
     app.patch("/allResults/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = { _id: new ObjectId(id) }
+      const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
       const term = req.body;
       const final = {
@@ -94,15 +101,13 @@ async function run() {
             Chemistry: parseInt(term.Chemistry),
             English: parseInt(term.English),
             Math: parseInt(term.Math),
-            Physics: parseInt(term.Physics)
-          }
-
-        }
-      }
+            Physics: parseInt(term.Physics),
+          },
+        },
+      };
       const result = await resultCollection.updateOne(filter, final, options);
-      res.send(result)
+      res.send(result);
     });
-
 
     app.post("/allResults", async (req, res) => {
       const newItem = req.body;
@@ -128,10 +133,14 @@ async function run() {
       res.send(result);
     });
 
-    app.post('/jwt', (req , res) => {
-      const user = req.body
-      const token = jwt.sign(user, 'c8f7271005d7cb49f09e71f9f24e8989f0126396a8a88cc1e232f15a130bf83e964e120940702b373c6fe477f69e1d9aa744e58dfc420c50f2794c424c688d91', {expiresIn: '100h'})
-      res.send(token)
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(
+        user,
+        "c8f7271005d7cb49f09e71f9f24e8989f0126396a8a88cc1e232f15a130bf83e964e120940702b373c6fe477f69e1d9aa744e58dfc420c50f2794c424c688d91",
+        { expiresIn: "100h" }
+      );
+      res.send(token);
     });
 
     app.get("/allResults/:examType/:classId", async (req, res) => {
@@ -171,65 +180,74 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/reCheck/:id", async (req, res) => {
+      const id = req.params.id;
+      const deleteId = { _id: new ObjectId(id) };
+      const deleteFromCollection = await usersCollection.deleteOne(deleteId);
+      res.send(deleteFromCollection);
+    });
+
     app.get("/reCheckUser", async (req, res) => {
       const email = req.query.email;
       const query = {
-        email: email
-      }
+        email: email,
+      };
       const result = await reCheckCollection.find(query).toArray();
       res.send(result);
     });
 
     //User Collections API
 
-    app.post('/addUser', async(req, res) => {
+    app.post("/addUser", async (req, res) => {
       const userInfo = req.body;
       const query = {
-        email : userInfo.email
-      }
+        email: userInfo.email,
+      };
       const existingUser = await usersCollection.findOne(query);
-      if(existingUser){
-        res.send({message : "User already added" });
-      }else{
+      if (existingUser) {
+        res.send({ message: "User already added" });
+      } else {
         const result = await usersCollection.insertOne(userInfo);
         res.send(result);
       }
-    })
+    });
 
     //Checking admin or not
-    app.get('/user/isAdmin',async(req, res) => {
+    app.get("/user/isAdmin", async (req, res) => {
       const email = req.query.email;
- 
-      const query = {email: email}
+
+      const query = { email: email };
       const user = await usersCollection.findOne(query);
-      const result = { admin: user?.role === 'admin'}
-    
-      return res.send(result)
-    })
+      const result = { admin: user?.role === "admin" };
+
+      return res.send(result);
+    });
 
     //Manage User
-    app.get('/userList', async(req, res) => {
+    app.get("/userList", async (req, res) => {
       const userlist = await usersCollection.find().toArray();
       res.send(userlist);
     });
 
-    app.patch('/userList/admin/:id', async (req, res) => {
+    app.patch("/userList/admin/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = { _id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
-          role: 'admin'
+          role: "admin",
         },
       };
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
-    app.delete('/deleteUser/:id', async(req, res) => {
+    app.delete("/deleteUser/:id", async (req, res) => {
       const id = req.params.id;
-      const deleteId = { _id : new ObjectId(id) };
+      const deleteId = { _id: new ObjectId(id) };
       const deleteFromCollection = await usersCollection.deleteOne(deleteId);
       res.send(deleteFromCollection);
-    })
+    });
+
+    //review Delete
 
     // Send a ping to confirm a successful connection
 
